2cb08da574df6fb4957d15cb6c4ba9b64fcb98ed
Charles Palmer <charlespalmer@hh1chp38.stunet.aber.ac.uk>
2017-11-20 15:24:11 +0000

    work on displaying the forum posts and replies - completed, need to make it look pretty.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,39 +2,29 @@ module PostsHelper
 
   # function to handle the recursive building of
   # replies
-  def build_replies(replies, post, html = '')
-    html = '<table>
-    <thead>
-    <tr>
-    <th>User</th>
-    <th>Parent</th>
-    <th>Post</th>
-    <th>Title</th>
-    <th>Text</th>
-    <th colspan="3"></th>
-    </tr>
-  </thead>
-    <tbody>'
+  def build_replies(responses, post, html = '', indent = 5)
 
-    replies.each do |reply|
-      html = html + "<tr>
-      <td> + #{reply.user.firstname + " " + reply.user.surname } + </td>"
-        if reply.parent
-            html = html + "<td> #{reply.parent.title}</td>"
-        else
-            html = html + "<td> -- </td>"
-        end
-            html = html + "<td>#{reply.post.title}</td>"
-      html = html + "<td>#{reply.title}</td>"
-      html = html + "<td>#{reply.text}</td>"
-      html = html + "<td>#{link_to 'Reply', new_reply_path}/post/#{post}/reply/#{reply.id}</td>"
-      if(reply.responses)
-        html = html + "<td>#{build_replies(reply.responses, html)}</td>"
+    # For each of the responses in the collection build the html for it
+    responses.each do |response|
+      html = html + "<div style='margin-left:#{indent}%;' class='response'>"
+      html = html + "<p>#{response.user.firstname + " " + response.user.surname }  </p>"
+      html = html + "<p><strong>#{response.title}</strong></p>"
+      html = html + "<p>#{response.text}</p>"
+
+      # Pre build the url - seperate it out of line below
+      url = "/post/#{post}/reply/#{response.id}"
+
+      html = html + "<p>  #{link_to 'Reply', new_reply_path + url}</p>"
+
+      html = html + "</div>"
+
+      # If this reply has responses, reccursivley call this function
+      # to keep building html
+      if(response.responses)
+        indent = indent + 5
+        html = build_replies(response.responses, post, html, indent)
       end
-      html = html + "</tr>"
     end
-    html = html + "</tbody>"
-    html = html + "</table>"
 
     html.html_safe
   end
