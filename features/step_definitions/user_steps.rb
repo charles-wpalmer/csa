2b508e33d196f61bf7442ca7365a35b8ed4ec70c
Charles Palmer <charlespalmer@hh1chp38.stunet.aber.ac.uk>
2017-11-29 11:50:39 +0000

    work on testing

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,15 +1,21 @@
-Given("that user {string} with password {string} has logged in") do |string, string2|
+Given('that user {string} with password {string} has logged in') do |string, string2|
   visit('/session/new?')
-  fill_in("login", :with => string)
-  fill_in("password", :with => string2)
-  click_button("login_button")
+  fill_in('login', :with => string)
+  fill_in('password', :with => string2)
+  click_button('login_button')
 end
 
-When("the user creates a new anonymous thread with the title {string} with the body {string}") do |title, body|
-    pending # Write code here that turns the phrase above into concrete actions
+When('the user creates a new anonymous thread with the title {string} with the body {string}') do |title, body|
+  visit('/users/new?page=1')
+
+  fill_in('post_title', :with => string)
+  fill_in('post[title]', :with => title)
+  fill_in('post[anonymous]', :with => true)
+
+  click_button('Create Post')
 end
 
-Then("the current page should contain a new row containing the data:") do |table|
+Then('the current page should contain a new row containing the data:') do |table|
     # table is a Cucumber::MultilineArgument::DataTable
     pending # Write code here that turns the phrase above into concrete actions
 end
