79705d7a9a77efa887b56c36d6b7c432f443d079
Charles Palmer <charlespalmer@hh1chp38.stunet.aber.ac.uk>
2017-12-03 13:38:52 +0000

    Renamed home_helper.rb file correctly, and ensured the recent posts from the home page user images arent clickable

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,14 +1,15 @@
 module HomeHelper
 
-  def image_for
+  def gen_image(user, size = :medium)
     if user.image
+
       user_image = user.image.photo.url(size)
       image_text = "Image of #{user.firstname} #{user.surname}"
+      
       image_tag(user_image, class: 'image-tag',
                         alt: image_text,
                         title: image_text, border: '0')
     else
-      # Creates a non-clickable default image
       image_tag("blank-cover_#{size}.png",
                 class: 'image-tag', alt: 'No photo available',
                 title: 'No photo available', border: '0')
