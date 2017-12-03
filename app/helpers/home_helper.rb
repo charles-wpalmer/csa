57e820f388df830bcb52d2d875d5f8e6b3c91654
Charles Palmer <charlespalmer@hh1chp38.stunet.aber.ac.uk>
2017-12-03 13:55:25 +0000

    Editied HomeHelper layout slightly for readability. And prevented Guests from viewing recent posts on the home page

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -5,7 +5,7 @@ module HomeHelper
 
       user_image = user.image.photo.url(size)
       image_text = "Image of #{user.firstname} #{user.surname}"
-      
+
       image_tag(user_image, class: 'image-tag',
                         alt: image_text,
                         title: image_text, border: '0')
