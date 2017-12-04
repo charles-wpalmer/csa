14b5a5dc865e94f4ffe31e014861319b0941b6cb
Charles Palmer <charlespalmer@hh1chp38.stunet.aber.ac.uk>
2017-12-04 11:43:21 +0000

    Removed update and delete functions from controllers/api/posts_controller - not needed

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -28,27 +28,6 @@ class API::PostsController < API::ApplicationController
     end
   end
 
-  # PATCH/PUT /posts/1.json
-  def update
-    respond_to do |format|
-      if @post.update(post_params)
-        format.json { render :show, status: :ok, location: @post }
-      else
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /posts/1.json
-  def destroy
-    respond_to do |format|
-      format.json do
-        @post.destroy
-        head :no_content
-      end
-    end
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_post
